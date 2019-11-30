53c14182101367aa1d0d3c61740b8e1be5487311
Bobi Basari <bobibasari@gmail.com>
2019-11-30 18:05:19 +0700

    update v1.3 fixed-version

--- src/redux/reducers/counterReducers.js
+++ src/redux/reducers/counterReducers.js
@@ -5,15 +5,15 @@ const INITIAL_STATE = 0;
 export default (state = INITIAL_STATE, action) => {
 	switch (action.type) {
 		case MOBIL:
-			console.log('reducer', action);
+			// console.log('reducer', action);
 			return state + action.payload * 2000;
 
 		case MOTOR:
-			console.log('reducer', action);
+			// console.log('reducer', action);
 			return state + action.payload * 1000;
 
 		case RESET:
-			console.log('reducer', action);
+			// console.log('reducer', action);
 			return state * 0;
 
 		default:
